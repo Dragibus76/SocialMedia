4a599a274867cc23f73cee52fccdb5d949a44844
CodeDragi <codedragi@gmail.com>
2022-09-11 12:25:06 +0200

    Frontend > Build Share Modal

--- src/components/RightSide/RightSide.jsx
+++ src/components/RightSide/RightSide.jsx
@@ -5,8 +5,12 @@ import Noti from "../../img/noti.png";
 import Comment from "../../img/comment.png";
 import { UilSetting } from "@iconscout/react-unicons";
 import TrendCard from '../TrendCard/TrendCard';
+import { useState } from 'react';
+import ProfileModal from '../ProfileModal/ProfileModal';
+import ShareModal from '../ShareModal/ShareModal';
 
 const RightSide = () => {
+  const [modalOpened, setModalOpened] = useState(false);
   return (
     <div className="RightSide">
         <div className="navIcons">
@@ -16,7 +20,11 @@ const RightSide = () => {
             <img src={Comment} alt="" />
         </div>
         <TrendCard/>
-        <button className="button r-button">Partager</button>
+        <button className="button r-button" onClick={() => setModalOpened(true)}
+        >
+         
+        Partager</button>
+        <ShareModal modalOpened={modalOpened} setModalOpened={setModalOpened}/> 
     </div>
   )
 }
